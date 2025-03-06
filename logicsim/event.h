abcefd7a2786ebe13816d0eb7aa14fd36aacb381
Olivia Liau <oliau@usc.edu>
2025-03-06 10:58:54 +0000

    first comm

--- logicsim/event.h
+++ logicsim/event.h
@@ -11,6 +11,12 @@ struct Event
 
 typedef struct EventLess {
         //write the operator() required to make this a functor that compares Events by time
+bool operator()(Event* E1, Event* E2) const {
+    if (E1 == nullptr) return false;
+    else if (E2 == nullptr) return true;
+    else if (E1->time < E2->time) return false;
+    else return true;
+}
 } EventLess;
 	
 #endif
